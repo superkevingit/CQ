3166a1afe6a39463c9d82c6e25aa264657b0973f
superkevingit <superkevingit@gmail.com>
2018-04-15 13:09:49 +0800

    fix before bugs

--- info.py
+++ info.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from peewee import *
-from msdatabase import *
+from ms_database import *
 from user import User
+from datetime import datetime
 
 class Info(BaseModel):
     id = PrimaryKeyField()
@@ -10,7 +11,7 @@ class Info(BaseModel):
     title = CharField()
     details = CharField()
     price = CharField()
-    time = DataTimeField(default=datetime.datetime.now)
+    time = DateTimeField(default=datetime.now)
     img_name = CharField(null=True)
     tag = CharField() #商品类型 如数码等
     flag = BooleanField(default=0) #商品类型 出售1或收购0
